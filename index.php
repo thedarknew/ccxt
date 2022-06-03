cd23115595d2e791f9a365cd9a596dffbc3dc5e3
thedarknew <peershao@gmail.com>
2022-06-03 09:32:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
 <?php
 
-$root = dirname(dirname(dirname(__FILE__)));
-
-include $root . '/ccxt.php';
-
-date_default_timezone_set('UTC');
-
-$exchange = new \ccxt\binance(array(
-    'timeout' => 30000,
-));
-
-try {
-
-    $symbol = 'ETH/BTC';
-    $result = $exchange->fetch_ticker ($symbol);
-
-    echo ($result);
-
-} catch (\ccxt\NetworkError $e) {
-    echo '[Network Error] ' . $e->getMessage() . "\n";
-} catch (\ccxt\ExchangeError $e) {
-    echo '[Exchange Error] ' . $e->getMessage() . "\n";
-} catch (Exception $e) {
-    echo '[Error] ' . $e->getMessage() . "\n";
-}
-echo "Ye";
+echo "Hello";
 ?>
